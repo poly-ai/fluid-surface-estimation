4e0a385bef4c8e01e3338acb5ad3cc8cac9f2480
blaze8871 <chunwei@umich.edu>
2022-02-26 15:50:20 -0500

    updata video code

--- src/visualization/video-output.py
+++ src/visualization/video-output.py
@@ -5,6 +5,7 @@ import os
 import glob
 import cv2
 
+INPUT_DATA = '../data/1D-waves/output/wave-sin-1.npy'
 OUTPUT_DIR = "./temp-frames"
 OUTPUT_FRAME_PREFIX = 'frame'
 VIDEO_FILENAME = 'output.mp4'
@@ -32,8 +33,9 @@ def get_point_height(x, y, data):
     return data[y, x]
 
 # Generate a single frame
-def generate_frame(single_frame_data, output_path):
-    height, width = single_frame_data.shape
+def generate_frame(single_frame_data, output_path, maxz, minz):
+    height = single_frame_data.shape[1];
+    width = single_frame_data.shape[0];
     X = np.arange(width)
     Y = np.arange(height)
     X, Y = np.meshgrid(X, Y)
@@ -49,23 +51,25 @@ def generate_frame(single_frame_data, output_path):
     ax1.set_xlabel('x axis')
     ax1.set_ylabel('y axis')
     ax1.set_zlabel('z axis')
+    ax1.set_zlim(minz,maxz)
     plt.savefig(output_path)
     plt.close()
 
 
 def main():
     # Load data
-    x_train = np.load('../data/1D-waves/output/x-train-adv.npy')
-    single_example = x_train[0,:,:,:] # 100,100, NUM_FRAMES
-    num_frames = single_example.shape[2]
-    print("Input data shape: ", single_example.shape)
+    data = np.load(INPUT_DATA)
+    num_frames = data.shape[0];
 
+    max_h = np.max(data);
+    min_h = np.min(data);
+        
     # Generate frame sequence for single example
     for i in range(num_frames):
-        single_frame = single_example[:,:,i]
+        single_frame = data[i,:,:]
         filename = f"{OUTPUT_FRAME_PREFIX}-{i:04}.png"
         output_path = os.path.join(OUTPUT_DIR, filename)
-        generate_frame(single_frame, output_path)
+        generate_frame(single_frame, output_path, max_h, min_h)
 
     # Generate video file
     img_array = []
